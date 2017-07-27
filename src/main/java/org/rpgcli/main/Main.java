6e282d5affd75211eaeaa11373de5744423f5fe3
Levi Franca <levifranca@gmail.com>
2017-07-26 23:02:07 -0300

    Add JUnit, JaCoCo and first tests.

--- src/main/java/org/rpgcli/main/Main.java
+++ src/main/java/org/rpgcli/main/Main.java
@@ -1,7 +1,9 @@
 package org.rpgcli.main;
 
+import org.rpgcli.utils.ConsoleWriter;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		ConsoleWriter.getInstance().writeColorText("Hello World!", "\u001B[31m");
 	}
 }
